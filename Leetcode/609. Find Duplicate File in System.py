3923c8a2be411b17485bb36b49938ed10029a660
BeamlakF <beamlakfekadu562@gmail.com>
2026-02-12 10:44:46 +0300

    leetcode questions

---
+++ Leetcode/609. Find Duplicate File in System.py
@@ -0,0 +1,17 @@
+def findDuplicate(self, paths):
+        content_map = {}
+        for path in paths:
+            parts = path.split(" ")
+            directory = parts[0]
+            for file in parts[1:]:
+                name, content = file.split("(")
+
+                full_path = directory + "/" + name
+                if content not in content_map:
+                    content_map[content] = []
+                content_map[content].append(full_path)
+            result = []
+        for file_list in content_map.values():
+            if len(file_list) > 1:
+                result.append(file_list)
+        return result
