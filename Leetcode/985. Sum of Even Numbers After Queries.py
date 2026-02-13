fa364f67b357f226ed03165b3afed0d917798d2c
BeamlakF <beamlakfekadu562@gmail.com>
2026-02-13 15:56:02 +0300

    leetcode questions

---
+++ Leetcode/985. Sum of Even Numbers After Queries.py
@@ -0,0 +1,13 @@
+def sumEvenAfterQueries(self, A, queries):
+        even_sum = sum(x for x in A if x % 2 == 0)
+        res = []
+        
+        for val, idx in queries:
+            if A[idx] % 2 == 0:
+                even_sum -= A[idx]
+            A[idx] += val
+            if A[idx] % 2 == 0:
+                even_sum += A[idx]
+            res.append(even_sum)
+        
+        return res
