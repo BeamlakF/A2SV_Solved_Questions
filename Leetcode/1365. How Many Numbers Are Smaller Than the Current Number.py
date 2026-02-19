dac52320747c46b5469d2f658c578ca1388b3dc2
BeamlakF <beamlakfekadu562@gmail.com>
2026-02-19 14:04:27 +0300

    leetcode questions

---
+++ Leetcode/1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,14 @@
+def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
+        count =[]
+        
+        n = len(nums)
+        for i in range(0, n):
+            count_num = 0
+            for j in range(0,n ):
+                if nums[j]<nums[i]:
+                    count_num +=1
+            count.append(count_num)
+        return count
+                    
+
+        
