736438bb1b0f8c8d63b9404785b3191cf5ed5d04
BeamlakF <beamlakfekadu562@gmail.com>
2026-03-04 17:55:13 +0300

    leetcode questions

---
+++ Leetcode/128. Longest Consecutive Sequence.py
@@ -0,0 +1,17 @@
+def longestConsecutive(self, nums) -> int:
+        num_set = set(nums)
+        max_length = 0
+        for num in num_set:
+            if num-1 not in num_set:
+                current = num
+                length = 1
+
+                while current + 1 in num_set:
+                    current +=1
+                    length +=1
+
+                max_length = max(max_length, length)
+
+        return max_length
+
+ 
