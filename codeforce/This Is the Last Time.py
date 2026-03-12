e6544026f9728592e5ed8519a2dbfe4af87d8025
BeamlakF <beamlakfekadu562@gmail.com>
2026-03-12 10:00:32 +0300

    codeforce questions

---
+++ codeforce/This Is the Last Time.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    casinos = []
+    for _ in range(n):
+        l, r, real = map(int, input().split())
+        casinos.append((l, r, real))
+    
+    casinos.sort()   
+    
+    coins = k
+    for l, r, real in casinos:
+        if l <= coins <= r: # to check if it playable, then coins can be equal to real, real is the maximum thing given in the arrays
+            coins = real
+    
+    
+    print(coins)
