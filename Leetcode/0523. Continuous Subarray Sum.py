7ab85c0af393511edcb32af4022e825d32cb2335
BeamlakF <beamlakfekadu562@gmail.com>
2026-03-15 22:55:47 +0300

    leetcode questions

--- Leetcode/0523. Continuous Subarray Sum.py
+++ Leetcode/0523. Continuous Subarray Sum.py
@@ -1,19 +1,17 @@
 from typing import List
 
 def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        look_up = {0:-1}
+        look_up = {0: -1}
         prefix_sum = 0
 
         for i in range(len(nums)):
             prefix_sum += nums[i]
-            check =prefix_sum%k
+            check = prefix_sum % k
 
             if check in look_up:
-                if i - look_up[check]>1:
+                if i - look_up[check] > 1:
                     return True
-                else:
-                    look_up[check] = i
+            else:
+                look_up[check] = i
 
         return False
-
-        
