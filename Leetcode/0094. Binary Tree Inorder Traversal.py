ac803bf901fadd152c2c1994e8cbbb3096ca90f6
BeamlakF <beamlakfekadu562@gmail.com>
2026-03-18 18:57:31 +0300

    codeforce questions

---
+++ Leetcode/0094. Binary Tree Inorder Traversal.py
@@ -0,0 +1,15 @@
+def inorderTraversal(self, root):
+        #in-order, left, parent and then right
+        order =[]
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            order.append(node.val)
+            dfs(node.right)
+
+            return
+
+        dfs(root)
+        return order        
+        
