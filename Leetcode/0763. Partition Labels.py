d32d00eef5753c8663300ee4f1365b9afa702471
BeamlakF <beamlakfekadu562@gmail.com>
2026-04-20 13:31:59 +0300

    leetcode questions

--- Leetcode/0763. Partition Labels.py
+++ Leetcode/0763. Partition Labels.py
@@ -4,15 +4,15 @@ def partitionLabels(self, s: str) -> List[int]:
         rightmost = {c: i for i, c in enumerate(s)}# to record the character's last occurence, but we will have to tack where it starts right?
         left =0
         right = 0
-        result = []
+        res = []
 
         for i, letter in enumerate(s):
             right = max(right, rightmost[letter])
 
             if i == right:
-                result.append(right - left + 1)
+                res.append(right - left + 1)
                 left = i + 1 # to start a new partition
 
-        return result
+        return res
 
 
