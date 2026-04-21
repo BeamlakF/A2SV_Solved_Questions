b813cee34571d14eb883740c5fe45912f79e5924
BeamlakF <beamlakfekadu562@gmail.com>
2026-04-21 19:13:54 +0300

    leet questions

---
+++ Leetcode/0992. Subarrays with K Different Integers.py
@@ -0,0 +1,27 @@
+def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        
+        def atMostK(a, k):
+            freq = defaultdict(int)
+            l = 0
+            distinct = 0
+            count = 0
+
+            for r in range(len(a)):
+                # expand window
+                if freq[a[r]] == 0:
+                    distinct += 1
+                freq[a[r]] += 1
+
+                # shrink if too many distinct
+                while distinct > k:
+                    freq[a[l]] -= 1
+                    if freq[a[l]] == 0:
+                        distinct -= 1
+                    l += 1
+
+                # count valid subarrays ending at r
+                count += (r - l + 1)
+
+            return count
+
+        return atMostK(nums, k) - atMostK(nums, k - 1)
