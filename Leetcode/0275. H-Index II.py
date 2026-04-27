66b2cd039cd176ab2178123398ce3722631e093e
BeamlakF <beamlakfekadu562@gmail.com>
2026-04-27 14:50:02 +0300

    leet questions

---
+++ Leetcode/0275. H-Index II.py
@@ -0,0 +1,12 @@
+def hIndex(self, citations) -> int:
+    n = len(citations)
+    left, right = 0, n - 1
+        
+    while left <= right:
+        mid = left + (right - left) // 2
+        if citations[mid] >= n - mid:
+            right = mid - 1
+        else:
+            left = mid + 1
+        
+    return n - left
